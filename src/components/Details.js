b319fcf78ca627dd7195429289b01c6a4b187c8a
timrusin <timrusin@gmail.com>
2022-09-22 17:05:21 -0500

    fixed call back return issue

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,7 +4,6 @@ import Projects from '../data/projectData'
 import MiniCard from './MiniCard'
 import './Details.css'
 
-console.log(Projects);
 
 const Details = () => {
     const [film, setFilm] = useState ({
@@ -19,11 +18,10 @@ const Details = () => {
     
     useEffect(()=> {
     Projects.forEach((item) => {
-     title === item.title && setFilm(item);
+     title === item.title && setFilm(item)
     });
 },)
 
-console.log(film);
   return (
     <div className="details-page-container">
       <h1 className='details-title'>{film.title}</h1>
@@ -31,8 +29,6 @@ console.log(film);
         <img src={film.image} alt={film.alt} className="detail-image"></img>
         <iframe
           className='youtube-embed'
-          // width="560"
-          // height="315"
           src={film.embedURL}
           title={film.title}
           frameborder="0"
@@ -43,9 +39,7 @@ console.log(film);
       <p className="detail-description">{film.description}</p>
         <div className='thumb-container'>
           {Projects.map(item => {
-            if(item.title !== title){
-            return <MiniCard {...item} key={item.title} />
-            }
+            return item.title !== title && <MiniCard {...item} key={item.title} />
           })}
         </div>
     </div>
