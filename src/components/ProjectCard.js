736eb87b4f12899b6ad068033d65c542acd32a91
timrusin <timrusin@gmail.com>
2022-09-22 17:07:43 -0500

    added jump to top function to ProjectCard as well to fix scrolling issue

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 
 
 const ProjectCard = (props) => {
+  const Top = () =>{
+    window.scrollTo({top:0, behavior: 'smooth'})
+}
   return (
     <div className='card'>
-        <Link to={`/details/${props.title}`} key={props.title}>
+        <Link to={`/details/${props.title}`} key={props.title} onClick={Top}>
             <div><img src={props.image} alt={props.alt} className='card-image'></img></div>
         </Link>
     </div>
